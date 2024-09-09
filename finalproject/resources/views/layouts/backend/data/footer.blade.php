66043db0faf103f8db5a11e6037af947731fcbd2
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2024-09-09 13:47:39 +0700

    delete project

--- finalproject/resources/views/layouts/backend/data/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        <p>&copy; Larisa Shop <script>document.write(new Date().getFullYear());</script>. Hak cipta dilindungi undang-undang</p>
-    </div>
-    <div class="footer-right">
-      2.3.0
-    </div>
-  </footer>
