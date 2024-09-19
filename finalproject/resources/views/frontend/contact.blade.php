fd2bf9493531fed50a986acafa457708cdbb4da3
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-19 20:39:54 +0700

    Update contact.blade.php

--- finalproject/resources/views/frontend/contact.blade.php
+++ finalproject/resources/views/frontend/contact.blade.php
@@ -19,7 +19,10 @@
     <section class="categories">
         <div class="container">
             <div class="row">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63090.23646955669!2d121.83915318182744!3d-8.772917267203429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dad4235ea432731%3A0x64c05a2b026f1ffa!2sKec.%20Wolowaru%2C%20Kabupaten%20Ende%2C%20Nusa%20Tenggara%20Tim.!5e0!3m2!1sid!2sid!4v1726062933040!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63090.23646955669!2d121.83915318182744!3d-8.772917267203429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dad4235ea432731%3A0x64c05a2b026f1ffa!2sKec.%20Wolowaru%2C%20Kabupaten%20Ende%2C%20Nusa%20Tenggara%20Tim.!5e0!3m2!1sid!2sid!4v1726062933040!5m2!1sid!2sid"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="col-lg-4 col-md-4 col-sm-4 p-0 ">
                     <div class="categories__item">
                         <div class="categories__text">
@@ -30,8 +33,8 @@
                     </div>
                 </div>
             </div>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
         </div>
-@endsection
+    @endsection
