1ae5ec1f92819dbfb43524705853fbe8b62bc53d
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-23 11:15:43 +0700

    delete master product create blade

--- finalproject/resources/views/backend/product/create.blade.php
+++ finalproject/resources/views/backend/product/create.blade.php
@@ -4,7 +4,7 @@
         <div class="col-12">
             @component('components.backend.card.card-form')
                 @slot('isfile', true)
-                @slot('action', Route('master.product.store'))
+                @slot('action', Route('product.store'))
                 @slot('method', 'POST')
                 @slot('content')
 
@@ -38,7 +38,7 @@
                     <x-forms.input type="file" name="thumbnails5" id="thumbnails5" label="Gambar 5" />
 
                     <div class="text-right">
-                        <a href="{{ Route('master.product.index') }}" class="btn btn-danger"
+                        <a href="{{ Route('product.index') }}" class="btn btn-danger"
                             href="#">{{ __('button.cancel') }}</a>
                         <button type="submit" class="btn btn-primary" href="#">{{ __('button.save') }}</button>
                     </div>
