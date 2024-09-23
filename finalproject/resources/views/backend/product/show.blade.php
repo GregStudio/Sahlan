cd789a0f84f64ed43a715171e2c1befce264e652
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-23 11:19:57 +0700

    delete master product show blade

--- finalproject/resources/views/backend/product/show.blade.php
+++ finalproject/resources/views/backend/product/show.blade.php
@@ -6,9 +6,9 @@
                 <div class="card-header">
                     <h4 class="card-title">{{ $data['product']->name }}</h4>
                     <div class="card-header-action">
-                        <a href="{{ route('master.product.edit', $data['product']->id) }}"
+                        <a href="{{ route('product.edit', $data['product']->id) }}"
                             class="btn btn-success">{{ __('button.edit') }}</a>
-                        <a href="{{ route('master.product.index') }}" class="btn btn-primary">{{ __('button.back') }}</a>
+                        <a href="{{ route('product.index') }}" class="btn btn-primary">{{ __('button.back') }}</a>
                     </div>
                 </div>
                 <div class="card-body">
