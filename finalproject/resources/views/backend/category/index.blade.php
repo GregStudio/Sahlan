075ed527a4e81c759c2baa625795cfdfaa0244c5
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-23 11:14:49 +0700

    delete master category index blade

--- finalproject/resources/views/backend/category/index.blade.php
+++ finalproject/resources/views/backend/category/index.blade.php
@@ -6,7 +6,7 @@
                 @slot('header')
                     <h4 class="card-title">{{ __('menu.category') }}</h4>
                     <div class="card-header-action">
-                        <a href="{{ route('master.category.create') }}" class="btn btn-primary">{{ __('button.add') }}
+                        <a href="{{ route('category.create') }}" class="btn btn-primary">{{ __('button.add') }}
                             {{ __('menu.category') }}</a>
                     </div>
                 @endslot
@@ -28,14 +28,14 @@
                             <td>{{ $category->Products()->count() }}</td>
                             <td>
                                 <x-button.dropdown-button :title="__('field.action')">
-                                    <a class="dropdown-item has-icon" href="{{ route('master.category.edit', $category->id) }}"><i
+                                    <a class="dropdown-item has-icon" href="{{ route('category.edit', $category->id) }}"><i
                                             class="far fa-edit"></i>
                                         {{ __('button.edit') }}</a>
-                                    <a class="dropdown-item has-icon" href="{{ route('master.category.show', $category->id) }}"><i
+                                    <a class="dropdown-item has-icon" href="{{ route('category.show', $category->id) }}"><i
                                             class="far fa-eye"></i>
                                         {{ __('button.detail') }}</a>
                                     <a class="dropdown-item has-icon btn-delete"
-                                        href="{{ route('master.category.delete', $category->id) }}"><i class="fa fa-trash"></i>
+                                        href="{{ route('category.delete', $category->id) }}"><i class="fa fa-trash"></i>
                                         {{ __('button.delete') }}</a>
                                 </x-button.dropdown-button>
                             </td>
