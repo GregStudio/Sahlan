f2822845a7db6679782f154cfcac8c9642c4da36
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-25 16:37:16 +0700

    update admin.product.restore

--- finalproject/resources/views/backend/product/deleted.blade.php
+++ finalproject/resources/views/backend/product/deleted.blade.php
@@ -34,7 +34,7 @@
                             <td>{{ tanggal($product->created_at) }}</td>
                             <td>
                                 <x-button.dropdown-button :title="__('field.action')">
-                                    <a class="dropdown-item has-icon" href="{{ route('product.restore', $product->id) }}"><i
+                                    <a class="dropdown-item has-icon" href="{{ route('admin.product.restore', $product->id) }}"><i
                                             class="fa fa-history"></i>
                                         Aktifkan</a>
                                 </x-button.dropdown-button>
