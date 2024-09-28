8a8b1ea4defeb6f9b7862ca0543ce700df3f9cf7
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-09-28 13:58:18 +0700

    update helpers

--- finalproject/app/Helpers/Helper.php
+++ finalproject/app/Helpers/Helper.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 function rupiah($number)
 {
-    return "Rp " . number_format($number,0,',','.');
+    return "Rp " . number_format($number, 0, ',', '.');
 }
 
-function tanggal($tanggal){
+function tanggal($tanggal)
+{
     return \Carbon\Carbon::parse($tanggal)->format('d F Y');
 }
