db03f4f6653a9596e3627f649923d0e26ed3610c
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-11-16 16:20:33 +0700

    Update ProductSeeder.php

--- finalproject/database/seeders/ProductSeeder.php
+++ finalproject/database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,6 +14,16 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::create([
+            'id'            => 1,
+            'categories_id' => 1,
+            'name'          => 'Nasi',
+            'slug'          => 'nasi',
+            'thumbnails'    => 'file/product/thumbnails/wsIiszQURHBMW95M6ezMXS1FuR2718Yfh3mYZZHC.jpg',
+            'price'         => 1000,
+            'weight'        => 1000,
+            'stock'         => 100,
+            'description'   => 'nasi'
+        ]);
     }
 }
