64484de581d94cdb5050c14e49cdaf837f1c264e
Franciscus Valentinus Ongkosianbhadra <120277436+franciscusvalentinus@users.noreply.github.com>
2024-11-25 12:11:27 +0700

    update price productseeder

--- finalproject/database/seeders/ProductSeeder.php
+++ finalproject/database/seeders/ProductSeeder.php
@@ -32,7 +32,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Beras Merah',
             'slug'          => 'beras-merah',
             'thumbnails'    => 'file/product/thumbnails/beras-merah.png',
-            'price'         => '15000',
+            'price'         => '20000',
             'weight'        => '1000',
             'stock'         => '100',
             'description'   => 'Beras Merah 1 Kg'
@@ -56,7 +56,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Telur Bebek',
             'slug'          => 'telur-bebek',
             'thumbnails'    => 'file/product/thumbnails/telur-bebek.jpg',
-            'price'         => '25000',
+            'price'         => '30000',
             'weight'        => '1000',
             'stock'         => '100',
             'description'   => 'Telur Bebek 1 Kg'
@@ -68,7 +68,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Minyak Goreng Bimoli 1 Liter',
             'slug'          => 'minyak-goreng-bimoli-1-liter',
             'thumbnails'    => 'file/product/thumbnails/minyak-goreng-bimoli-1-liter.jpg',
-            'price'         => '15000',
+            'price'         => '20000',
             'weight'        => '1000',
             'stock'         => '100',
             'description'   => 'Minyak Goreng Bimoli 1 Liter'
@@ -80,7 +80,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Minyak Goreng Bimoli 2 Liter',
             'slug'          => 'minyak-goreng-bimoli-2-liter',
             'thumbnails'    => 'file/product/thumbnails/minyak-goreng-bimoli-2-liter.jpg',
-            'price'         => '30000',
+            'price'         => '35000',
             'weight'        => '2000',
             'stock'         => '100',
             'description'   => 'Minyak Goreng Bimoli 2 Liter'
@@ -92,7 +92,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Bawang Merah',
             'slug'          => 'bawang-merah',
             'thumbnails'    => 'file/product/thumbnails/bawang-merah.jpeg',
-            'price'         => '25000',
+            'price'         => '40000',
             'weight'        => '1000',
             'stock'         => '100',
             'description'   => 'Bawang Merah 1 Kg'
@@ -104,7 +104,7 @@ class ProductSeeder extends Seeder
             'name'          => 'Bawang Putih',
             'slug'          => 'bawang-putih',
             'thumbnails'    => 'file/product/thumbnails/bawang-putih.jpeg',
-            'price'         => '25000',
+            'price'         => '45000',
             'weight'        => '1000',
             'stock'         => '100',
             'description'   => 'Bawang Putih 1 Kg'
