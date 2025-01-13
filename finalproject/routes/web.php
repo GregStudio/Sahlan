8e82002ffd97e264ec9fabbecb104272b3ec9ae5
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-01-13 10:14:13 +0700

    Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Reapply "Update web.php"""""""""""""""""""""
    This reverts commit b0b08810a6b7773573ad5c1a23f414b94fe41672.

--- finalproject/routes/web.php
+++ finalproject/routes/web.php
@@ -14,16 +14,7 @@ use App\Http\Controllers\Rajaongkir\RajaongkirController;
 use App\Http\Controllers\Setting\WebconfigController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::prefix('app')->group(function () {
     Route::middleware(['auth'])->group(function () {
